f687f2c24ff5d4ee68253b7cef33f5a76c31a5d5
atikiNBTW <robinmusy12@gmail.com>
2024-10-24 14:58:06 +0500

    restore resetting minecraft option

--- src/main/java/com/atikinbtw/chatonlostfocus/mixins/MinecraftClientMixin.java
+++ src/main/java/com/atikinbtw/chatonlostfocus/mixins/MinecraftClientMixin.java
@@ -4,7 +4,9 @@ import com.atikinbtw.chatonlostfocus.config.ConfigScreen;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.option.GameOptions;
 import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,9 +18,13 @@ public abstract class MinecraftClientMixin {
     @Shadow @Nullable public Screen currentScreen;
     @Shadow public abstract void setScreen(@Nullable Screen screen);
 
+    @Shadow @Final public GameOptions options;
+
     @Inject(method = "onWindowFocusChanged", at = @At("HEAD"))
     private void onWindowFocusChanged(boolean focused, CallbackInfo ci) {
-        if (!focused && ConfigScreen.config.enabled && this.currentScreen == null)
+        if (!focused && ConfigScreen.config.enabled && this.currentScreen == null) {
             this.setScreen(new ChatScreen(ConfigScreen.config.textInChat));
+            this.options.pauseOnLostFocus = false;
+        }
     }
 }
